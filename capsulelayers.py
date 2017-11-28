ce41f3afcb1fec448706dac34f377f0ca0cedd44
xifeng <guoxifeng1990@163.com>
2017-11-28 22:19:02 +0800

    works

---
+++ capsulelayers.py
@@ -0,0 +1,106 @@
+"""
+Some key layers used for constructing a Capsule Network. These layers can used to construct CapsNet on other dataset,
+not just on MNIST.
+
+Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Pytorch`
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def squash(inputs, axis=-1):
+    """
+    The non-linear activation used in Capsule. It drives the length of a large vector to near 1 and small vector to 0
+    :param inputs: vectors to be squashed
+    :param axis: the axis to squash
+    :return: a Tensor with same size as inputs
+    """
+    norm = torch.norm(inputs, p=2, dim=axis, keepdim=True)
+    scale = norm**2 / (1 + norm**2) / (norm + 1e-8)
+    return scale * inputs
+
+
+class DenseCapsule(nn.Module):
+    """
+    The dense capsule layer. It is similar to Dense (FC) layer. Dense layer has `in_num` inputs, each is a scalar, the
+    output of the neuron from the former layer, and it has `out_num` output neurons. DenseCapsule just expands the
+    output of the neuron from scalar to vector. So its input size = [None, in_num_caps, in_dim_caps] and output size = \
+    [None, out_num_caps, out_dim_caps]. For Dense Layer, in_dim_caps = out_dim_caps = 1.
+
+    :param in_num_caps: number of cpasules inputted to this layer
+    :param in_dim_caps: dimension of input capsules
+    :param out_num_caps: number of capsules outputted from this layer
+    :param out_dim_caps: dimension of output capsules
+    :param routings: number of iterations for the routing algorithm
+    """
+    def __init__(self, in_num_caps, in_dim_caps, out_num_caps, out_dim_caps, routings=3):
+        super(DenseCapsule, self).__init__()
+        self.in_num_caps = in_num_caps
+        self.in_dim_caps = in_dim_caps
+        self.out_num_caps = out_num_caps
+        self.out_dim_caps = out_dim_caps
+        self.routings = routings
+        self.weight = nn.Parameter(0.01 * torch.randn(out_num_caps, in_num_caps, out_dim_caps, in_dim_caps))
+
+    def forward(self, x):
+        # x.size=[batch, in_num_caps, in_dim_caps]
+        # expanded to    [batch, 1,            in_num_caps, in_dim_caps,  1]
+        # weight.size   =[       out_num_caps, in_num_caps, out_dim_caps, in_dim_caps]
+        # torch.matmul: [out_dim_caps, in_dim_caps] x [in_dim_caps, 1] -> [out_dim_caps, 1]
+        # => x_hat.size =[batch, out_num_caps, in_num_caps, out_dim_caps]
+        x_hat = torch.squeeze(torch.matmul(self.weight, x[:, None, :, :, None]), dim=-1)
+
+        # In forward pass, `x_hat_detached` = `x_hat`;
+        # In backward, no gradient can flow from `x_hat_detached` back to `x_hat`.
+        x_hat_detached = x_hat.detach()
+
+        # The prior for coupling coefficient, initialized as zeros.
+        # b.size = [batch, out_num_caps, in_num_caps]
+        b = Variable(torch.zeros(x.size(0), self.out_num_caps, self.in_num_caps)).cuda()
+
+        assert self.routings > 0, 'The \'routings\' should be > 0.'
+        for i in range(self.routings):
+            # c.size = [batch, out_num_caps, in_num_caps]
+            c = F.softmax(b, dim=1)
+
+            # At last iteration, use `x_hat` to compute `outputs` in order to backpropagate gradient
+            if i == self.routings - 1:
+                # c.size expanded to [batch, out_num_caps, in_num_caps, 1           ]
+                # x_hat.size     =   [batch, out_num_caps, in_num_caps, out_dim_caps]
+                # => outputs.size=   [batch, out_num_caps, 1,           out_dim_caps]
+                outputs = squash(torch.sum(c[:, :, :, None] * x_hat, dim=-2, keepdim=True))
+                # outputs = squash(torch.matmul(c[:, :, None, :], x_hat))  # alternative way
+            else:  # Otherwise, use `x_hat_detached` to update `b`. No gradients flow on this path.
+                outputs = squash(torch.sum(c[:, :, :, None] * x_hat_detached, dim=-2, keepdim=True))
+                # outputs = squash(torch.matmul(c[:, :, None, :], x_hat_detached))  # alternative way
+
+                # outputs.size       =[batch, out_num_caps, 1,           out_dim_caps]
+                # x_hat_detached.size=[batch, out_num_caps, in_num_caps, out_dim_caps]
+                # => b.size          =[batch, out_num_caps, in_num_caps]
+                b = b + torch.sum(outputs * x_hat_detached, dim=-1)
+
+        return torch.squeeze(outputs, dim=-2)
+
+
+class PrimaryCapsule(nn.Module):
+    """
+    Apply Conv2D with `out_channels` and then reshape to get capsules
+    :param in_channels: input channels
+    :param out_channels: output channels
+    :param dim_caps: dimension of capsule
+    :param kernel_size: kernel size
+    :return: output tensor, size=[batch, num_caps, dim_caps]
+    """
+    def __init__(self, in_channels, out_channels, dim_caps, kernel_size, stride=1, padding=0):
+        super(PrimaryCapsule, self).__init__()
+        self.dim_caps = dim_caps
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+
+    def forward(self, x):
+        outputs = self.conv2d(x)
+        outputs = outputs.view(x.size(0), -1, self.dim_caps)
+        return squash(outputs)
+
---
+++ capsulenet.py
@@ -0,0 +1,162 @@
+import torch
+from torch import nn
+from torch.optim import Adam, lr_scheduler
+from torch.autograd import Variable
+from torchvision import transforms, datasets
+from capsulelayers import DenseCapsule, PrimaryCapsule
+
+
+class CapsuleNet(nn.Module):
+    def __init__(self, input_size, classes, routings):
+        super(CapsuleNet, self).__init__()
+        self.input_size = input_size
+        self.classes = classes
+        self.routings = routings
+        self.conv1 = nn.Conv2d(input_size[0], 256, kernel_size=9, stride=1, padding=0)
+        self.primarycaps = PrimaryCapsule(256, 256, 8, kernel_size=9, stride=2, padding=0)
+        self.digitcaps = DenseCapsule(in_num_caps=32*6*6, in_dim_caps=8,
+                                      out_num_caps=classes, out_dim_caps=16, routings=routings)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(16*classes, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, input_size[0] * input_size[1] * input_size[2]),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x, y=None):
+        x = self.relu(self.conv1(x))
+        x = self.primarycaps(x)
+        x = self.digitcaps(x)
+        length = x.norm(dim=-1)
+        if y is None: # during testing, no label given
+            _, index = length.max(dim=1)
+            y = Variable(torch.zeros(length.size()).scatter_(1, index.view(-1, 1).cpu().data, 1.).cuda())
+        reconstruction = self.decoder((x * y[:, :, None]).view(x.size(0), -1))
+        return length, reconstruction.view(-1, *self.input_size)
+
+
+def caps_loss(y_pred, y_true, x_recon, x):
+    L = y_true * torch.clamp(0.9 - y_pred, min=0.) ** 2 + \
+        0.5 * (1 - y_true) * torch.clamp(y_pred - 0.1, min=0.) ** 2
+    L_margin = L.sum(dim=1).mean()
+
+    L_recon = nn.MSELoss()(x_recon, x)
+
+    return L_margin + 0.0005 * L_recon
+
+
+def test(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for x, y in test_loader:
+        y = torch.zeros(y.size(0), 10).scatter_(1, y.view(-1, 1), 1.)
+        x, y = Variable(x.cuda(), volatile=True), Variable(y.cuda())
+        y_pred, x_recon = model(x)
+        test_loss += caps_loss(y_pred, y, x_recon, x).data[0] * x.size(0) # sum up batch loss
+        y_pred = y_pred.data.max(1)[1]
+        y_true = y.data.max(1)[1]
+        correct += y_pred.eq(y_true).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    return test_loss, correct / len(test_loader.dataset)
+
+
+def train(model, train_loader, test_loader, args):
+    print('Begin Training' + '-'*70)
+    from time import time
+    t0 = time()
+    optimizer = Adam(model.parameters(), lr=args.lr)
+    lr_decay = lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    for epoch in range(args.epochs):
+        model.train()
+        lr_decay.step()
+        ti = time()
+        training_loss = 0.0
+        for i, (x, y) in enumerate(train_loader):
+            y = torch.zeros(y.size(0), 10).scatter_(1, y.view(-1, 1), 1.)
+            x, y = Variable(x.cuda()), Variable(y.cuda())
+
+            optimizer.zero_grad()
+            y_pred, x_recon = model(x, y)
+            loss = caps_loss(y_pred, y, x_recon, x)
+            loss.backward()
+            training_loss += loss.data[0] * x.size(0)
+            optimizer.step()
+
+        val_loss, val_acc = test(model, test_loader)
+
+        print("==> Epoch %02d: loss=%.5f, val_loss=%.5f, val_acc=%.4f, time=%ds"
+              % (epoch, training_loss / len(train_loader.dataset),
+                 val_loss, val_acc, time() - ti))
+        torch.save(model.state_dict(), args.save_dir + '/epoch%d.pkl' % epoch)
+    print("Total time = %ds" % (time() - t0))
+    print('End Training' + '-' * 70)
+
+def load_mnist(path='./data', download=False, batch_size=100, shift_pixels=2):
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(path, train=True, download=download,
+                       transform=transforms.Compose([transforms.RandomCrop(size=28, padding=shift_pixels),
+                                                     transforms.ToTensor()])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(path, train=False, download=download,
+                       transform=transforms.ToTensor()),
+        batch_size=batch_size, shuffle=True, **kwargs)
+
+    return train_loader, test_loader
+
+
+if __name__ == "__main__":
+    import argparse, os
+
+    # setting the hyper parameters
+    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch_size', default=100, type=int)
+    parser.add_argument('--lam_recon', default=0.0005, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")  # num_routing should > 0
+    parser.add_argument('--shift_pixels', default=2, type=int,
+                        help="Number of pixels to shift at most in each direction.")
+    parser.add_argument('--data_dir', default='./data',
+                        help="Directory of data. If no data, use \'--download\' flag to download it")
+    parser.add_argument('--download', action='store_true',
+                        help="Download the required data.")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--weights', default=None,
+                        help="The path of the saved weights. Should be specified when testing")
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    args = parser.parse_args()
+    print(args)
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # load data
+    train_loader, test_loader = load_mnist(args.data_dir, download=False, batch_size=args.batch_size)
+
+    # define model
+    model = CapsuleNet(input_size=[1, 28, 28], classes=10, routings=3)
+    model.cuda()
+    print(model)
+
+    # train or test
+    if args.weights is not None:  # init the model weights with provided one
+        model.load_state_dict(torch.load(args.weights))
+    if not args.testing:
+        train(model, train_loader, test_loader, args)
+    else:  # testing
+        if args.weights is None:
+            print('No weights are provided. Will test using random initialized weights.')
+        test_loss, test_acc = test(model=model, test_loader=test_loader)
+        print('test acc = %.4f, test loss = %.5f' % (test_acc, test_loss))
