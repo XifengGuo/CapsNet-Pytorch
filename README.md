5c6260b87a66c9108aed5a714e57266cf0b1e68d
xifeng <guoxifeng1990@163.com>
2017-11-30 01:30:26 +0800

    verified

---
+++ README.md
@@ -0,0 +1,128 @@
+# CapsNet-Pytorch
+[![License](https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000)](https://github.com/XifengGuo/CapsNet-Keras/blob/master/LICENSE)
+
+A Pytorch implementation of CapsNet in the paper:   
+[Sara Sabour, Nicholas Frosst, Geoffrey E Hinton. git. NIPS 2017](https://arxiv.org/abs/1710.09829)   
+The current `average test error = 0.34%` and `best test error = 0.30%`.   
+ 
+**Differences with the paper:**   
+- We use the learning rate decay with `decay factor = 0.9` and `step = 1 epoch`,    
+while the paper did not give the detailed parameters (or they didn't use it?).
+- We only report the test errors after `50 epochs` training.   
+In the paper, I suppose they trained for `1250 epochs` according to Figure A.1?
+- We use MSE (mean squared error) as the reconstruction loss and 
+the coefficient for the loss is `lam_recon=0.0005*784=0.392`.   
+This should be **equivalent** to using SSE (sum squared error) and `lam_recon=0.0005` as in the paper.
+
+
+**TODO**
+- Conduct experiments on other datasets. 
+- Explore interesting characteristics of CapsuleNet.
+
+**Contacts**
+- Your contributions to the repo are always welcome. 
+Open an issue or contact me with E-mail `guoxifeng1990@163.com` or WeChat `wenlong-guo`.
+
+
+## Usage
+
+**Step 1.
+Install [Pytorch](https://github.com/pytorch/pytorch) from source**
+
+I'm using the source code of Nov 24, 2017. The newest code should be working too.   
+Go https://github.com/pytorch/pytorch for installation instructions.
+
+**Step 2. Clone this repository to local.**
+```
+git clone https://github.com/XifengGuo/CapsNet-Pytorch.git
+cd CapsNet-Pytorch
+```
+
+**Step 3. Train a CapsNet on MNIST**  
+
+Training with default settings:
+```
+python capsulenet.py
+```
+
+Launching the following command for detailed usage:
+```
+python capsulenet.py -h
+``` 
+
+**Step 4. Test a pre-trained CapsNet model**
+
+Suppose you have trained a model using the above command, then the trained model will be
+saved to `result/trained_model.pkl`. Now just launch the following command to get test results.
+```
+python capsulenet.py --testing --weights result/trained_model.pkl
+```
+It will output the testing accuracy and show the reconstructed images.
+The testing data is same as the validation data. It will be easy to test on new data, 
+just change the code as you want.
+
+You can also just *download a model I trained* from 
+https://pan.baidu.com/s/1dFLFtT3
+
+
+## Results
+
+**Test Errors**   
+
+CapsNet classification test **error** on MNIST. Average and standard deviation results are
+reported by 3 trials. The results can be reproduced by launching the following commands.   
+ ```
+ python capsulenet.py --routings 1 #CapsNet-v1   
+ python capsulenet.py --routings 3 #CapsNet-v2
+```
+   Method     |   Routing   |   Reconstruction  |  MNIST (%)  |  *Paper*    
+   :---------|:------:|:---:|:----:|:----:
+   Baseline |  -- | -- | --             | *0.39* 
+   CapsNet-v1  |  1 | yes | 0.36 (0.016)| *0.29 (0.011)*
+   CapsNet-v2  |  3 | yes| 0.34 (0.029) | *0.25 (0.005)*
+   
+Losses and accuracies:   
+![](result/log.png)
+
+
+**Training Speed**  
+
+About `73s / epoch` on a single GTX 1070 GPU.   
+About `43s / epoch` on a single GTX 1080Ti GPU.         
+
+**Reconstruction result**  
+
+The result of CapsNet-v2 by launching   
+```
+python capsulenet.py --testing --weights result/trained_model.pkl
+```
+Digits at top 5 rows are real images from MNIST and 
+digits at bottom are corresponding reconstructed images.
+
+![](result/real_and_recon.png)
+
+
+## Other Implementations
+- Keras:   
+  - [XifengGuo/CapsNet-Keras](https://github.com/XifengGuo/CapsNet-Keras)   
+  
+- TensorFlow:
+  - [naturomics/CapsNet-Tensorflow](https://github.com/naturomics/CapsNet-Tensorflow.git)   
+  I referred to some functions in this repository.
+  - [InnerPeace-Wu/CapsNet-tensorflow](https://github.com/InnerPeace-Wu/CapsNet-tensorflow)   
+  - [chrislybaer/capsules-tensorflow](https://github.com/chrislybaer/capsules-tensorflow)
+
+- PyTorch:
+  - [timomernick/pytorch-capsule](https://github.com/timomernick/pytorch-capsule)
+  - [gram-ai/capsule-networks](https://github.com/gram-ai/capsule-networks)
+  - [nishnik/CapsNet-PyTorch](https://github.com/nishnik/CapsNet-PyTorch.git)
+  - [leftthomas/CapsNet](https://github.com/leftthomas/CapsNet)
+  
+- MXNet:
+  - [AaronLeong/CapsNet_Mxnet](https://github.com/AaronLeong/CapsNet_Mxnet)
+  
+- Chainer:
+  - [soskek/dynamic_routing_between_capsules](https://github.com/soskek/dynamic_routing_between_capsules)
+
+- Matlab:
+  - [yechengxi/LightCapsNet](https://github.com/yechengxi/LightCapsNet)
--- capsulenet.py
+++ capsulenet.py
@@ -1,3 +1,18 @@
+"""
+Pytorch implementation of CapsNet in paper Dynamic Routing Between Capsules.
+The current version maybe only works for TensorFlow backend. Actually it will be straightforward to re-write to TF code.
+Adopting to other backends should be easy, but I have not tested this.
+
+Usage:
+       Launch `python CapsNet.py -h` for usage help
+
+Result:
+    Validation accuracy > 99.6% after 50 epochs.
+    Speed: About 73s/epoch on a single GTX1070 GPU card and 43s/epoch on a GTX1080Ti GPU.
+
+Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Pytorch`
+"""
+
 import torch
 from torch import nn
 from torch.optim import Adam, lr_scheduler
@@ -7,16 +22,32 @@ from capsulelayers import DenseCapsule, PrimaryCapsule
 
 
 class CapsuleNet(nn.Module):
+    """
+    A Capsule Network on MNIST.
+    :param input_size: data size = [channels, width, height]
+    :param classes: number of classes
+    :param routings: number of routing iterations
+    Shape:
+        - Input: (batch, channels, width, height), optional (batch, classes) .
+        - Output:((batch, classes), (batch, channels, width, height))
+    """
     def __init__(self, input_size, classes, routings):
         super(CapsuleNet, self).__init__()
         self.input_size = input_size
         self.classes = classes
         self.routings = routings
+
+        # Layer 1: Just a conventional Conv2D layer
         self.conv1 = nn.Conv2d(input_size[0], 256, kernel_size=9, stride=1, padding=0)
+
+        # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_caps, dim_caps]
         self.primarycaps = PrimaryCapsule(256, 256, 8, kernel_size=9, stride=2, padding=0)
+
+        # Layer 3: Capsule layer. Routing algorithm works here.
         self.digitcaps = DenseCapsule(in_num_caps=32*6*6, in_dim_caps=8,
                                       out_num_caps=classes, out_dim_caps=16, routings=routings)
 
+        # Decoder network.
         self.decoder = nn.Sequential(
             nn.Linear(16*classes, 512),
             nn.ReLU(inplace=True),
@@ -25,6 +56,7 @@ class CapsuleNet(nn.Module):
             nn.Linear(1024, input_size[0] * input_size[1] * input_size[2]),
             nn.Sigmoid()
         )
+
         self.relu = nn.ReLU()
 
     def forward(self, x, y=None):
@@ -32,24 +64,55 @@ class CapsuleNet(nn.Module):
         x = self.primarycaps(x)
         x = self.digitcaps(x)
         length = x.norm(dim=-1)
-        if y is None: # during testing, no label given
-            _, index = length.max(dim=1)
+        if y is None:  # during testing, no label given. create one-hot coding using `length`
+            index = length.max(dim=1)[1]
             y = Variable(torch.zeros(length.size()).scatter_(1, index.view(-1, 1).cpu().data, 1.).cuda())
         reconstruction = self.decoder((x * y[:, :, None]).view(x.size(0), -1))
         return length, reconstruction.view(-1, *self.input_size)
 
 
-def caps_loss(y_pred, y_true, x_recon, x):
+def caps_loss(y_true, y_pred, x, x_recon, lam_recon):
+    """
+    Capsule loss = Margin loss + lam_recon * reconstruction loss.
+    :param y_true: true labels, one-hot coding, size=[batch, classes]
+    :param y_pred: predicted labels by CapsNet, size=[batch, classes]
+    :param x: input data, size=[batch, channels, width, height]
+    :param x_recon: reconstructed data, size is same as `x`
+    :param lam_recon: coefficient for reconstruction loss
+    :return: Variable contains a scalar loss value.
+    """
     L = y_true * torch.clamp(0.9 - y_pred, min=0.) ** 2 + \
         0.5 * (1 - y_true) * torch.clamp(y_pred - 0.1, min=0.) ** 2
     L_margin = L.sum(dim=1).mean()
 
     L_recon = nn.MSELoss()(x_recon, x)
 
-    return L_margin + 0.0005 * L_recon
+    return L_margin + lam_recon * L_recon
 
 
-def test(model, test_loader):
+def show_reconstruction(model, test_loader, n_images, args):
+    import matplotlib.pyplot as plt
+    from utils import combine_images
+    from PIL import Image
+    import numpy as np
+
+    model.eval()
+    for x, _ in test_loader:
+        x = Variable(x[:min(n_images, x.size(0))].cuda(), volatile=True)
+        _, x_recon = model(x)
+        data = np.concatenate([x.data, x_recon.data])
+        img = combine_images(np.transpose(data, [0, 2, 3, 1]))
+        image = img * 255
+        Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
+        print()
+        print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
+        print('-' * 70)
+        plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png", ))
+        plt.show()
+        break
+
+
+def test(model, test_loader, args):
     model.eval()
     test_loss = 0
     correct = 0
@@ -57,7 +120,7 @@ def test(model, test_loader):
         y = torch.zeros(y.size(0), 10).scatter_(1, y.view(-1, 1), 1.)
         x, y = Variable(x.cuda(), volatile=True), Variable(y.cuda())
         y_pred, x_recon = model(x)
-        test_loss += caps_loss(y_pred, y, x_recon, x).data[0] * x.size(0) # sum up batch loss
+        test_loss += caps_loss(y, y_pred, x, x_recon, args.lam_recon).data[0] * x.size(0)  # sum up batch loss
         y_pred = y_pred.data.max(1)[1]
         y_true = y.data.max(1)[1]
         correct += y_pred.eq(y_true).cpu().sum()
@@ -67,37 +130,69 @@ def test(model, test_loader):
 
 
 def train(model, train_loader, test_loader, args):
+    """
+    Training a CapsuleNet
+    :param model: the CapsuleNet model
+    :param train_loader: torch.utils.data.DataLoader for training data
+    :param test_loader: torch.utils.data.DataLoader for test data
+    :param args: arguments
+    :return: The trained model
+    """
     print('Begin Training' + '-'*70)
     from time import time
+    import csv
+    logfile = open(args.save_dir + '/log.csv', 'w')
+    logwriter = csv.DictWriter(logfile, fieldnames=['epoch', 'loss', 'val_loss', 'val_acc'])
+    logwriter.writeheader()
+
     t0 = time()
     optimizer = Adam(model.parameters(), lr=args.lr)
     lr_decay = lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    best_val_acc = 0.
     for epoch in range(args.epochs):
-        model.train()
-        lr_decay.step()
+        model.train()  # set to training mode
+        lr_decay.step()  # decrease the learning rate by multiplying a factor `gamma`
         ti = time()
         training_loss = 0.0
-        for i, (x, y) in enumerate(train_loader):
-            y = torch.zeros(y.size(0), 10).scatter_(1, y.view(-1, 1), 1.)
-            x, y = Variable(x.cuda()), Variable(y.cuda())
+        for i, (x, y) in enumerate(train_loader):  # batch training
+            y = torch.zeros(y.size(0), 10).scatter_(1, y.view(-1, 1), 1.)  # change to one-hot coding
+            x, y = Variable(x.cuda()), Variable(y.cuda())  # convert input data to GPU Variable
 
-            optimizer.zero_grad()
-            y_pred, x_recon = model(x, y)
-            loss = caps_loss(y_pred, y, x_recon, x)
-            loss.backward()
-            training_loss += loss.data[0] * x.size(0)
-            optimizer.step()
+            optimizer.zero_grad()  # set gradients of optimizer to zero
+            y_pred, x_recon = model(x, y)  # forward
+            loss = caps_loss(y, y_pred, x, x_recon, args.lam_recon)  # compute loss
+            loss.backward()  # backward, compute all gradients of loss w.r.t all Variables
+            training_loss += loss.data[0] * x.size(0)  # record the batch loss
+            optimizer.step()  # update the trainable parameters with computed gradients
 
-        val_loss, val_acc = test(model, test_loader)
-
+        # compute validation loss and acc
+        val_loss, val_acc = test(model, test_loader, args)
+        logwriter.writerow(dict(epoch=epoch, loss=training_loss / len(train_loader.dataset),
+                                val_loss=val_loss, val_acc=val_acc))
         print("==> Epoch %02d: loss=%.5f, val_loss=%.5f, val_acc=%.4f, time=%ds"
               % (epoch, training_loss / len(train_loader.dataset),
                  val_loss, val_acc, time() - ti))
-        torch.save(model.state_dict(), args.save_dir + '/epoch%d.pkl' % epoch)
+        if val_acc > best_val_acc:  # update best validation acc and save model
+            best_val_acc = val_acc
+            torch.save(model.state_dict(), args.save_dir + '/epoch%d.pkl' % epoch)
+            print("best val_acc increased to %.4f" % best_val_acc)
+    logfile.close()
+    torch.save(model.state_dict(), args.save_dir + '/trained_model.pkl')
+    print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
     print("Total time = %ds" % (time() - t0))
     print('End Training' + '-' * 70)
+    return model
+
 
 def load_mnist(path='./data', download=False, batch_size=100, shift_pixels=2):
+    """
+    Construct dataloaders for training and test data. Data augmentation is also done here.
+    :param path: file path of the dataset
+    :param download: whether to download the original data
+    :param batch_size: batch size
+    :param shift_pixels: maximum number of pixels to shift in each direction
+    :return: train_loader, test_loader
+    """
     kwargs = {'num_workers': 1, 'pin_memory': True}
 
     train_loader = torch.utils.data.DataLoader(
@@ -114,15 +209,16 @@ def load_mnist(path='./data', download=False, batch_size=100, shift_pixels=2):
 
 
 if __name__ == "__main__":
-    import argparse, os
+    import argparse
+    import os
 
     # setting the hyper parameters
     parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--batch_size', default=100, type=int)
-    parser.add_argument('--lam_recon', default=0.0005, type=float,
+    parser.add_argument('--lam_recon', default=0.0005 * 784, type=float,
                         help="The coefficient for the loss of decoder")
-    parser.add_argument('--routings', default=3, type=int,
+    parser.add_argument('-r', '--routings', default=3, type=int,
                         help="Number of iterations used in routing algorithm. should > 0")  # num_routing should > 0
     parser.add_argument('--shift_pixels', default=2, type=int,
                         help="Number of pixels to shift at most in each direction.")
@@ -131,9 +227,9 @@ if __name__ == "__main__":
     parser.add_argument('--download', action='store_true',
                         help="Download the required data.")
     parser.add_argument('--save_dir', default='./result')
-    parser.add_argument('--testing', action='store_true',
+    parser.add_argument('-t', '--testing', action='store_true',
                         help="Test the trained model on testing dataset")
-    parser.add_argument('--weights', default=None,
+    parser.add_argument('-w', '--weights', default=None,
                         help="The path of the saved weights. Should be specified when testing")
     parser.add_argument('--lr', default=0.001, type=float,
                         help="Initial learning rate")
@@ -158,5 +254,6 @@ if __name__ == "__main__":
     else:  # testing
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
-        test_loss, test_acc = test(model=model, test_loader=test_loader)
+        test_loss, test_acc = test(model=model, test_loader=test_loader, args=args)
         print('test acc = %.4f, test loss = %.5f' % (test_acc, test_loss))
+        show_reconstruction(model, test_loader, 50, args)
